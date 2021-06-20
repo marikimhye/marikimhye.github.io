9e0c99f620b765949d8878b569bbedfc48607b34
marikimhye <marikimhye@gamil.com>
2021-06-20 16:58:24 +0900

    change about

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -7,8 +7,8 @@ var documents = [{
     }, {
     "id": 1,
     "url": "https://marikimhye.github.io/about",
-    "title": "Mediumish Template for Jekyll",
-    "body": "This website is built with Jekyll and Mediumish template for Jekyll. It's for demonstration purposes, no real content can be found. Mediumish template for Jekyll is compatible with Github pages, in fact even this demo is created with Github Pages and hosted with Github.  Documentation: Please, read the docs here. Questions or bug reports?: Head over to our Github repository! Buy me a coffeeThank you for your support! Your donation helps me to maintain and improve Mediumish . Buy me a coffee Documentation"
+    "title": "About Marie",
+    "body": "      This website is built with Jekyll and Mediumish template for Jekyll. It's for demonstration purposes, no real content can be found. Mediumish template for Jekyll is compatible with Github pages, in fact even this demo is created with Github Pages and hosted with Github.           "
     }, {
     "id": 2,
     "url": "https://marikimhye.github.io/categories",
