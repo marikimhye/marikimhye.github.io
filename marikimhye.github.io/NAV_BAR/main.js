dba946782c9b6f84571f04c0f21ae89fd3c4b13b
marikimhye <marikimhye@gamil.com>
2021-06-20 13:32:16 +0900

    add navbar

---
+++ marikimhye.github.io/NAV_BAR/main.js
@@ -0,0 +1,8 @@
+const toogleBtn = document.querySelector('.navbar__toogleBtn');
+const menu = document.querySelector('.navbar__menu');
+
+toogleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+
